027551140dabc61413fed77e37385dda444ebce0
Mantas <man.petrikas@gmail.com>
2018-12-14 13:52:41 +0200

    added encoder decoder

---
+++ src/decoder.py
@@ -0,0 +1,6 @@
+class Encoder:
+    def __init__(self,matrix):
+        self.matrix = matrix
+
+    def decode(self, message):
+        return ""
---
+++ src/encoder.py
@@ -0,0 +1,6 @@
+class Encoder:
+    def __init__(self,matrix):
+        self.matrix = matrix
+
+    def encode(self, message):
+        return ""
